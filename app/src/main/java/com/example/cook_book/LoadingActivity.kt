0245b95a16239055a409ea7f8a38d1d7e497dbbe
Sparhawk96 <petersoncade96@gmail.com>
2021-11-18 17:06:46 -0700

    Added Hacked Loading Screen

--- app/src/main/java/com/example/cook_book/LoadingActivity.kt
+++ app/src/main/java/com/example/cook_book/LoadingActivity.kt
@@ -1,50 +1,28 @@
 package com.example.cook_book
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
 
 class LoadingActivity : AppCompatActivity() {
-    private lateinit var kRecyclerView: RecyclerView
-    val recipeList = recipeModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.loading_activity)
 
-        kRecyclerView = findViewById(R.id.recipe_recycler_view) as RecyclerView
-        kRecyclerView.layoutManager = LinearLayoutManager(this)
-
-        // Adapter here
-        val kadapter: recipeAdapter = recipeAdapter(this, recipeList)
-
-        kRecyclerView.setAdapter(kadapter)
-
-
-        // add
-        val addButton: Button = findViewById(R.id.addButton)
-        var acount: Int = 0
-        addButton.setOnClickListener {
-            acount += 1
-            recipeList.add(recipeCard("aNewRecipe", false))
-            kadapter.notifyDataSetChanged()
+        //TODO: Add code to load recipes here and remove button and use loading bar
+        var button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            Log.d("RECIPE", "Finished Loading.")
+            Log.d("RECIPE", "Transitioning to RecipeListActivity")
+            val intent = Intent(this, RecipeListActivity::class.java)
+            startActivity(intent)
         }
-
-
-        // Remove
-        val removeButton: Button = findViewById(R.id.removeButton)
-        removeButton.setOnClickListener {
-            for (i in recipeList.size downTo 1) {
-                if (recipeList[i - 1].selected) {
-                    recipeList.removeAt(i - 1)
-                }
-            }
-            kadapter.notifyDataSetChanged()
-
-        }
-
-
     }
 }
