1c381ea9443db72fef142bfc2b0894a69456b7ac
Sparhawk96 <petersoncade96@gmail.com>
2021-11-22 23:22:14 -0700

    Added Layout Fixes

--- app/src/main/java/com/example/cook_book/recipe/RecipeIngredientsAdaptor.kt
+++ app/src/main/java/com/example/cook_book/recipe/RecipeIngredientsAdaptor.kt
@@ -7,10 +7,11 @@ import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.cook_book.R
-import com.example.cook_book.Recipe
+import com.example.cook_book.RecipeAdapter
 import kotlin.math.ceil
+import kotlin.reflect.KFunction2
 
-class RecipeIngredientsAdaptor(var ingredients: List<String>) : RecyclerView.Adapter<RecipeIngredientsAdaptor.Line>() {
+class RecipeIngredientsAdaptor(val onClickListener: View.OnClickListener, var ingredients: List<String>) : RecyclerView.Adapter<RecipeIngredientsAdaptor.Line>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Line {
         val view = LayoutInflater.from(parent.getContext()).inflate(R.layout.ingredient_entry, parent, false)
@@ -18,24 +19,40 @@ class RecipeIngredientsAdaptor(var ingredients: List<String>) : RecyclerView.Ada
     }
 
     override fun onBindViewHolder(line: Line, position: Int) {
-        val loc = position * 2
+        val loc = position * 3
         line.apply {
-            if(ingredients.size == loc + 1){
-                rightSide.visibility = View.GONE
-                leftSideText.text = ingredients[loc]
+            first.alpha = 1.0f
+            second.alpha = 1.0f
+            third.alpha = 1.0f
+
+            if(loc + 3 <= ingredients.size) {
+                firstText.text = ingredients[loc]
+                secondText.text = ingredients[loc + 1]
+                thirdText.text = ingredients[loc + 2]
+            }else if (loc + 2 == ingredients.size){
+                firstText.text = ingredients[loc]
+                secondText.text = ingredients[loc + 1]
+                third.alpha = 0.0f
             }else{
-                leftSideText.text = ingredients[loc]
-                rightSideText.text = ingredients[loc + 1]
+                firstText.text = ingredients[loc]
+                second.alpha = 0.0f
+                third.alpha = 0.0f
             }
         }
     }
 
-    override fun getItemCount(): Int = ceil(ingredients.size / 2.0).toInt()
+    override fun getItemCount(): Int = ceil(ingredients.size / 3.0).toInt()
 
     inner class Line(val view: View) : RecyclerView.ViewHolder(view) {
-        val rightSide = view.findViewById<LinearLayout>(R.id.right_side)
+        val first = view.findViewById<LinearLayout>(R.id.first_line)
+        val firstText = first.findViewById<TextView>(R.id.ingredient_text)
+        val second = view.findViewById<LinearLayout>(R.id.second_line)
+        val secondText = second.findViewById<TextView>(R.id.ingredient_text)
+        val third = view.findViewById<LinearLayout>(R.id.third_line)
+        val thirdText = third.findViewById<TextView>(R.id.ingredient_text)
 
-        val leftSideText = view.findViewById<TextView>(R.id.left_text)
-        val rightSideText = view.findViewById<TextView>(R.id.right_text)
+            init {
+            view.setOnClickListener(onClickListener)
+        }
     }
 }
